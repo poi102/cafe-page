82f38ea511d6953d8ec1f0448988b34fd4c287fe
poi102 <ni10mu09@gamil.com>
2026-03-23 18:03:43 +0900

    forest-cafe

---
+++ src/Contact.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact: React.FC = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Address: 123 Cafe Street</p>
+      <p>Phone: (123) 456-7890</p>
+    </div>
+  );
+};
+
+export default Contact;
